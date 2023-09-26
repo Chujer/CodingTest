dd6912d5d0cd5f0dddd8d651a895c21cfc1c1c98
Chujer <113624740+Chujer@users.noreply.github.com>
2023-09-26 17:28:07 +0900

    2023.09.25~26

--- programmers/Level2/Project1/쿼드압축 후 개수 세기.cpp
+++ programmers/Level2/Project1/쿼드압축 후 개수 세기.cpp
@@ -1,49 +1,57 @@
 #include <string>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
-void Search(vector<vector<int>> arr, int x, int y, int size, int& answer)
+void Search(vector<vector<int>>& arr, int x, int y, int size, vector<int>& answer)
 {
-    vector<vector<int>> tempArr(size, vector<int>(size));
-    int count = 0;
+    int count = arr[y][x];
+    bool isX = false;
 
     for (int i = y; i < y + size; i++)
     {
         for (int j = x; j < x + size; j++)
         {
-            count += arr[i][j];
+            if (count != arr[i][j])
+            {
+                isX = true;
+                break;
+            }
         }
+        if (isX)
+            break;
     }
 
-    if (count > 0 && count != size * 2)
+    if (isX)
     {
-        for (int i = y; i < y + size; i++)
-        {
-            vector<int> temp;
-            for (int j = x; j < x +size; j++)
-            {
-                temp.push_back(arr[i][j]);
-            }
-            tempArr.push_back(temp);
-        }
-
         for (int i = 0; i < 2; i++)
         {
             for (int j = 0; j < 2; j++)
             {
-                Search(arr, i * size / 2, j / size / 2, size / 2, answer);
+                Search(arr, size * j / 2 + x, size * i / 2 + y, size / 2,answer);
             }
         }
-    }
 
-    answer++;
+    }
+    else if (count == 1)
+    {
+        answer[1]++;
+    }
+    else if (count == 0)
+        answer[0]++;
 }
 
 vector<int> solution(vector<vector<int>> arr) {
-    vector<int> answer;
+    vector<int> answer(2);
 
-    Search(arr, 0,0,arr.size(), )
+    Search(arr, 0, 0, arr.size(), answer);
+
 
     return answer;
+}
+
+int main()
+{
+    solution({{1, 1, 1, 1, 1, 1, 1, 1}, {0, 1, 1, 1, 1, 1, 1, 1}, {0, 0, 0, 0, 1, 1, 1, 1}, {0, 1, 0, 0, 1, 1, 1, 1}, {0, 0, 0, 0, 0, 0, 1, 1}, {0, 0, 0, 0, 0, 0, 0, 1}, {0, 0, 0, 0, 1, 0, 0, 1}, {0, 0, 0, 0, 1, 1, 1, 1}} );
 }
