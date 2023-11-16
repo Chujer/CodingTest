e0ac82ed6648a309df4f6b6c0622c0803fe195e7
Chujer <113624740+Chujer@users.noreply.github.com>
2023-11-16 23:59:36 +0900

    2023.11.16

---
+++ programmers/Level3/Level3/N으로 표현.cpp
@@ -0,0 +1,57 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+int solution(int N, int number) {
+    int answer = 0;
+
+    vector<int> DP(32001, 32000);
+
+    DP[0] = 2;
+    DP[1] = 2;
+    for (int i = 1; i < 5; i++)
+    {
+        string indexStr = "";
+        for (int j = 1; j <= i; j++)
+        {
+            indexStr += N + '0';
+        }
+
+        int index = stoi(indexStr);
+
+        if (index > DP.size())
+            continue;
+        DP[index] = indexStr.size();
+        if (index * index < DP.size())
+             DP[index * index] = indexStr.size() * 2;
+    }
+
+    for (int i = 2; i < DP.size(); i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            if(i+j < DP.size())
+                DP[i + j] = min(DP[i + j], DP[i] + DP[j]);
+            if(i-j >= 0)
+                DP[i - j] = min(DP[i - j], DP[i] + DP[j]);
+            if(i / j >= 0)
+                DP[i / j] = min(DP[i / j], DP[i] + DP[j]);
+            if(i*j < DP.size())
+                DP[i * j] = min(DP[i * j], DP[i] + DP[j]);
+        }
+    }
+
+    answer = DP[number];
+
+    if (answer == 32000)
+        answer = -1;
+
+    return answer;
+}
+
+int main()
+{
+    solution(8, 53);
+}
