e5824cda0f87a21a9c8576969b1fce516d2b4801
Chujer <113624740+Chujer@users.noreply.github.com>
2024-05-10 16:53:47 +0900

    2024.05.10
    프로그래머스 - NQueen

--- programmers/Level2/Project1/N-Queen.cpp
+++ programmers/Level2/Project1/N-Queen.cpp
@@ -5,75 +5,39 @@
 #include <set>
 
 using namespace std;
-int boardSize = 0;
-using namespace std;
-map<pair<int,int>, int> check;
 
-bool isAble(vector<vector<int>>& data, pair<int, int> pos)
+void DFS(vector<int>& data, int& answer, int col) 
 {
-    int x = pos.first;
-    int y = pos.second;
-
-    for (int i = 0; i < data.size(); i++)
+    if (data.size() == col) 
     {
-        if (data[y][i] == 1)
-            return false;
-        if (data[i][x] == 1)
-            return false;
+        answer++;
     }
-
-    for (int i = boardSize; i > -boardSize; i--)
+    else 
     {
-        if (y + i >= 0 && x + i >= 0 && y + i < boardSize && x + i < boardSize)
-            if (data[y + i][x + i] == 1)
-                return false;
-        if (y - i >= 0 && y - i < boardSize && x + i >= 0 && x + i < boardSize)
-            if (data[y - i][x + i] == 1)
-                return false;
-    }
-    return true;
-}
-
-
-void DFS(vector<vector<int>>& data, int n, set<map<pair<int, int>, int>>& test)
-{
-
-    vector<vector<int>> curData = data;
-    map<pair<int, int>, int> curCheck = check;
-
-    if (n == boardSize)
-    {
-        test.insert(check);
-        return;
-    }
-
-    for (int i = 0; i < boardSize; i++)
-    {
-        for (int j = 0; j < boardSize; j++)
+        for (int i = 0; i < data.size(); i++) 
         {
-            if (check.find({ j, i }) != check.end())
-                continue;
-            if (!isAble(data, { j,i }))
-                continue;
+            bool flag = true;
+            data[col] = i;
 
-            data[i][j] = 1;
-            check[{j, i}] = 1;
-
-            DFS(data,n + 1,  test);
-            data = curData;
-            check = curCheck;
+            for (int j = 0; j < col; j++) 
+            {
+                if (data[col] == data[j] || abs(data[col] - data[j]) == abs(col - j)) 
+                {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) 
+                DFS(data, answer, col + 1);
         }
     }
 }
 
-
 int solution(int n) {
     int answer = 0;
-    vector<vector<int>> data(n,vector<int>(n));
-    boardSize = n;
-    set<map<pair<int, int>, int>> test;
-    DFS(data, 0, test);
-    answer = test.size();
+    vector<int> data(n);
+    DFS(data, answer, 0);
+
 
     return answer;
 }
